94ea8370ad51e7c1a65dbd90b557ef093ff9d235
Oleksandra Ostroskaya <zubarevaoleksandra05@gmail.com>
2023-10-02 23:31:11 +0300

    modal hamburger

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,15 @@
 const btnOpen = document.querySelector('.bussines__button')
 const modal = document.querySelector('.js-backdrop')
 const btnClose = document.querySelector('.modal__close')
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector('.nav')
+
 
 function openModal () {
 	modal.classList.add('showModal')
 }
 function closeModal (event) {
+	event.preventDefault()
 	if (event.target === event.currentTarget) {
 		modal.classList.remove('showModal')
 	}
@@ -15,3 +19,12 @@ function closeModal (event) {
 btnClose.onclick = closeModal
 modal.onclick = closeModal
 btnOpen.onclick = openModal
+
+
+    if (hamburger !== null) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+					menu.classList.toggle('is-active')
+        }, false);
+				
+    }
